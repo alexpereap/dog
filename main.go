ca2a59376135f641e772f5d4c994fa25091c5be6
Alex Perea <ing.alexperea@mail.com>
2025-05-31 13:57:22 -0500

    Second

--- main.go
+++ main.go
@@ -7,3 +7,7 @@ import (
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says " + strings.ToUpper(s)
 }
+
+func WhenSmall(s string) string {
+	return "When the puppy is small it says " + strings.ToLower(s)
+}
